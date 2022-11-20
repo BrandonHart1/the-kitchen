22db4e10484fc9542854538d8fc6559be6039149
Brandon_Hart <sugb73@gmail.com>
2022-11-20 14:54:00 -0500

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import { useRef } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import '../styles/main.css';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const navRef = useRef();
+
+  const showNavBar = () => {
+    navRef.current.classList.toggle('responsive_nav');
+  };
+
+  return (
+    <header>
+      <h2>The Kitchen</h2>
+      <nav ref={navRef}>
+        <a href='/#'>Home</a>
+        <a href='/#'>Menu</a>
+        <a href='/#'>Locations</a>
+        <a href='/#'>About</a>
+        <button className='nav-btn nav-close-btn' onClick={showNavBar}>
+          <FaTimes />
+        </button>
+      </nav>
+      <button className='nav-btn' onClick={showNavBar}>
+        <FaBars />
+      </button>
+    </header>
+  );
 };
 
 export default Navbar;
